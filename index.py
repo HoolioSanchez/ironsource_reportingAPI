e0016470c7c9f4783e6932749e49084cfe98063a
Julio Sanchez <juliosanchez@juliosad0bmhv2g.lan>
2019-07-03 09:55:13 -0700

    additional columns added to DF

--- index.py
+++ index.py
@@ -8,8 +8,10 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-start_date = '2019-06-26'
-end_date = '2019-06-26'
+start_date = '2019-06-01'
+end_date = '2019-06-30'
+placementAdRevFilename = 'export (68).csv'
+days_in_month = 30
 irn = ironsource()
 #%%
 def getAllAppData(start_date, end_date, breakdown):
@@ -33,7 +35,7 @@ def extractDataFromDFColumn(dataframe):
     new_df[col_target] = col
 
     #Cleaning columns
-    new_df['Impressions'] = [imp['impressions'] for imp in new_df.data]
+#     new_df['Impressions'] = [imp['impressions'] for imp in new_df.data]
     new_df['clicks'] = [imp['clicks'] for imp in new_df.data]
     new_df['clickThroughRate'] = [imp['clickThroughRate'] for imp in new_df.data]
     # new_df['VideoCompletions'] = [imp['videoCompletions'] for imp in new_df.data]
@@ -49,23 +51,58 @@ def extractDataFromDFColumn(dataframe):
 data = getAllAppData(start_date, end_date, breakdown = 'placement')
 
 #%%
-cleanData = extractDataFromDFColumn(data)
+placementRevenue = pd.DataFrame.from_csv(placementAdRevFilename)
+placementRevenue = placementRevenue.reset_index()
 
-cleanData = cleanData.set_index('date')
-cleanData = cleanData.drop('appKey', axis = 1)
+cleanData = extractDataFromDFColumn(data)
 cleanData = cleanData.drop('bundleId', axis = 1)
 cleanData = cleanData.reset_index()
 
 #%%
-cleanData.head()
+cleanDF = cleanData
+cleanDF['Date'] = pd.to_datetime(cleanDF['date'])
+cleanDF = cleanDF.set_index('Date')
+cleanDF['appkey'] = cleanDF['appKey']
 
-jsonData = cleanData.to_json(orient='records')
+#%%
+monthlyDF = cleanDF.groupby(['appName','appkey','placement']).resample('m').mean()
+monthly_sum = cleanDF.groupby(['appName','appkey','placement']).resample('m').sum()
+monthlyDF.reset_index(inplace=True)
+monthly_sum.reset_index(inplace=True)
+monthlyDF['placement'] = monthlyDF['placement'].str.lower()
+monthly_sum['placement'] = monthly_sum['placement'].str.lower()
+
+monthlyDF['avg_engagedUsers'] = monthlyDF['engagedUsers']
+monthlyDF['avg_clicks'] = monthlyDF['clicks']
+
+monthlyDF.drop('engagedUsers', axis = 1, inplace=True)
+monthlyDF.drop('clicks', axis = 1, inplace=True)
+
+#%%
+monthlyDF['engagedUsers'] = monthly_sum['engagedUsers']
+monthlyDF['clicks'] = monthly_sum['clicks']
+
+
+#%%
+placementDF = pd.merge(placementRevenue,monthlyDF, on=['placement','appkey'])
+#%%
+# placementDF['AppName'] = [app for item in placementDF['appkey'] for app in apps if item == apps[app]]
+
+placementDF.head()
+#%%
+finalDF = placementDF[['appName', 'placement','market','revenue','clicks','clickThroughRate','engagedUsers','engagementRate','Impression','impressionsPerEngagedUser','eCPMs']]
+
+#%%
+finalDF['ARPDEU'] = (finalDF['revenue']/days_in_month)/finalDF['avg_engagedUsers']
+
+#%%
+jsonData = finalDF.to_json(orient='records')
 cleanJson = json.loads(jsonData)
 
 #%%
-cleanData.head()
-#%%
+print(cleanJson)
 
+#%%
 config = credentials['googleSheets']
 scope = ['https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive']
@@ -78,7 +115,7 @@ def addRowToGoogleSheets(data):
     wks.append_row(data, value_input_option='USER_ENTERED' )
 #%%
 def filterRows(index):
-    rows = [index['date'], index['adUnits'], index['appName'], index['placement'], index['Impressions'], index['clicks'], index['clickThroughRate'], index['engagedUsers'], index['engagementRate'], index['impressionsPerEngagedUser']]
+    rows = [index['appName'], index['placement'], index['market'], index['revenue'], index['eCPMs'], index['ARPDEU'], index['Impression'], index['clicks'], index['clickThroughRate'], index['engagedUsers'], index['engagementRate'], index['impressionsPerEngagedUser']]
     return rows
 
 def appendRow(data):
@@ -86,4 +123,7 @@ def appendRow(data):
         rows = filterRows(index)
         addRowToGoogleSheets(rows)
 
-appendRow(cleanJson)
+#%%
+finalDF.to_csv('placementData.csv')
+# appendRow(cleanJson)
+#%%
